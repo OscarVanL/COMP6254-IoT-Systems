de2907d2fad799f85ab3043d1213c623f55459af
OscarVanL <7999692+OscarVanL@users.noreply.github.com>
2020-12-31 01:57:24 +0000

    Added histogram binned labels for PIR triggered and door opened times

--- coursework/CourseworkClient.py
+++ coursework/CourseworkClient.py
@@ -1,7 +1,7 @@
 import uuid
 from dataclasses import dataclass
 import yaml
-from datetime import datetime
+from datetime import datetime, timedelta
 import os
 import csv
 import time
@@ -34,11 +34,10 @@ class CourseworkClient:
         self.last_fridge_opened = None
 
         # Uncomment to replay saved CSV messages to Graphite
-        #replay_csv(self)
+        # replay_csv(self)
 
         # Start new threads for each broker
         self.ttn_broker.loop_forever()
-
 
     def on_subscribe(self, mosq, obj, mid, granted_qos):
         print('----------------')
@@ -50,7 +49,6 @@ class CourseworkClient:
 
     def on_publish(self, client, userdata, mid):
         print("Published")
-
 
     def on_disconnect(self, client, userdata, rc):
         if rc != 0:
@@ -81,16 +79,9 @@ class CourseworkClient:
         print("Logging to CSV")
         log_to_csv(payload)
 
-
     def relay_to_grafana(self, payload: KitchenData):
 
         interval = 120
-        # Round fridge opened and PIR triggered time to nearest minute
-        fridge_opened_rounded = int(payload.fridge_opened_time.timestamp()//60 * 60)
-        pir_triggered_rounded = int(payload.PIR_triggered_time.timestamp()//60 * 60)
-
-
-
         graphite_data = [
             {
                 "name": "kitcheniot.meta.rssi",
@@ -142,33 +133,65 @@ class CourseworkClient:
             }
         ]
 
+        # Round fridge opened and PIR triggered time to nearest minute
+        fridge_opened_rounded = round_minute(payload.fridge_opened_time)
+        pir_triggered_rounded = round_minute(payload.PIR_triggered_time)
+        # Graphite histogram bins:
+        hist_bins = ['0000', '0030', '0100', '0130', '0200', '0230', '0300', '0330', '0400', '0430', '0500', '0530',
+                     '0600', '0630', '0700', '0730', '0800', '0830', '0900', '0930', '1000', '1030', '1100', '1130',
+                     '1200', '1230', '1300', '1330', '1400', '1430', '1500', '1530', '1600', '1630', '1700', '1730',
+                     '1800', '1830', '1900', '1930', '2000', '2030', '2100', '2130', '2200', '2230', '2300', '2330']
+
         # Don't send duplicate timestamps
         if fridge_opened_rounded != self.last_fridge_opened:
             self.last_fridge_opened = fridge_opened_rounded
+            h = fridge_opened_rounded.hour
+            m = fridge_opened_rounded.minute
+            index = int((h * 60 + m) / 30)
+
             graphite_data.append({
                 "name": "kitcheniot.activity.fridge",
                 "value": 1,
                 "interval": interval,
                 "unit": "",
-                "time": fridge_opened_rounded,
+                "time": int(fridge_opened_rounded.timestamp()),
+                "mtype": "gauge",
+            })
+            graphite_data.append({
+                "name": "kitcheniot.activity.fridge.{}".format(hist_bins[index]),
+                "value": 1,
+                "interval": interval,
+                "unit": "",
+                "time": int(fridge_opened_rounded.timestamp()),
                 "mtype": "gauge",
             })
 
         if pir_triggered_rounded != self.last_PIR_triggered:
             self.last_PIR_triggered = pir_triggered_rounded
+            h = pir_triggered_rounded.hour
+            m = pir_triggered_rounded.minute
+            index = int((h * 60 + m) / 30)
             graphite_data.append({
                 "name": "kitcheniot.activity.pir",
                 "value": 1,
                 "interval": interval,
                 "unit": "",
-                "time": pir_triggered_rounded,
+                "time": int(pir_triggered_rounded.timestamp()),
+                "mtype": "gauge",
+            })
+            graphite_data.append({
+                "name": "kitcheniot.activity.pir.{}".format(hist_bins[index]),
+                "value": 1,
+                "interval": interval,
+                "unit": "",
+                "time": int(pir_triggered_rounded.timestamp()),
                 "mtype": "gauge",
             })
 
-
         print("graphite data: ", graphite_data)
 
-        result = requests.post(self.config['GRAPHITE_URL'], auth=(self.config['GRAPHITE_USER'], self.config['GRAPHITE_API_KEY']), json=graphite_data)
+        result = requests.post(self.config['GRAPHITE_URL'],
+                               auth=(self.config['GRAPHITE_USER'], self.config['GRAPHITE_API_KEY']), json=graphite_data)
         if result.status_code != 200:
             raise Exception(result.text)
         print('%s: %s' % (result.status_code, result.text))
@@ -221,3 +244,7 @@ def replay_csv(client: CourseworkClient):
             time.sleep(0.1)  # Leave a small delay so we don't spam Graphite
 
 
+def round_minute(date_time):
+    # Rounds to nearest minute by adding a timedelta if seconds>=30
+    return (date_time.replace(second=0, microsecond=0, minute=date_time.minute)
+            + timedelta(minutes=date_time.second // 30))
